6f8c57357186d2884252d28389defaf618d16f39
Jonas Lins <jal3@cin.ufpe.br>
2015-09-18 11:24:06 -0300

    [#103455154] Refactored SignUpResource

--- Project/src/main/java/com/software/project/service/rest/resource/SignUpResource.java
+++ Project/src/main/java/com/software/project/service/rest/resource/SignUpResource.java
@@ -10,6 +10,7 @@ import org.restlet.resource.ServerResource;
 import com.google.gson.Gson;
 import com.software.project.entities.User;
 import com.software.project.service.UserService;
+import com.software.project.service.rest.resource.LoginResource.Status;
 import com.software.project.util.ApplicationContextProvider;
 
 public class SignUpResource extends ServerResource {
@@ -23,7 +24,7 @@ public class SignUpResource extends ServerResource {
 	@Post
 	public synchronized Representation createEntity(final String representation) throws Exception {
 		Gson gson = new Gson();
-		String responseStr = null;
+		Status status = null;
 
 		// Getting URL request parameters keys
 		String username = getQuery().getValues("username");
@@ -32,17 +33,21 @@ public class SignUpResource extends ServerResource {
 		// BikePosition msg = gson.fromJson(representation, BikePosition.class);
 
 		User user = userService.getUserByUsername(username);
+		User userEmail = userService.getUserByEmail(email);
 		
 		if (user != null) {
-			responseStr = gson.toJson(new SignUpResponse(Status.USERNAME_EXISTS));
-			return new JsonRepresentation(responseStr);
-		}else{
+			status = Status.USERNAME_EXISTS;
+		}else if(userEmail!=null){
+			status = Status.USER_EMAIL_EXISTS;
+		}else{		
 			User newUser = new User(username, password, email);
 			userService.createUser(newUser);
 			
-			responseStr = gson.toJson(new SignUpResponse(Status.USER_SUCCESSFUL_REGISTERED));
-			return new JsonRepresentation(responseStr);
+			status = Status.USER_SUCCESSFUL_REGISTERED;			
 		}
+		
+		String responseStr = gson.toJson(new SignUpResponse(status));
+		return new JsonRepresentation(responseStr);
 
 	}
 
@@ -57,7 +62,7 @@ public class SignUpResource extends ServerResource {
 	}
 
 	public enum Status {
-		USER_SUCCESSFUL_REGISTERED(1), USERNAME_EXISTS(2), TOKEN_NOT_VALID(3);
+		USER_SUCCESSFUL_REGISTERED(1), USERNAME_EXISTS(2), USER_EMAIL_EXISTS(3), TOKEN_NOT_VALID(4);
 
 		private int value;
 
